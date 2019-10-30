9888c0f0a04f18a17278db8fdaf98e3eecc751e0
Sayantan <sayantan.world98@gmail.com>
2019-10-30 18:04:37 +0530

    gprof uploaded

--- 04gcov_gprof/gprof/loops.c
+++ 04gcov_gprof/gprof/loops.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 void f1(){
         int i;
-        printf("------F1------\n");
-        for(i=0; i<=0xfff123ff; i++) {
+        printf("------F1 running------\n");
+        for(i=0; i<=0x7ff123ff; i++) {
         }
 }
 void f2(){
         int i;
-        printf("----F2-----\n");
+        printf("------F2 running------\n");
         for(i=0; i<=0xf2f1ffff; i++) {
         }
 }
 void f3(){
         int i;
-        printf("----F3-----\n");
+        printf("------F3 running------\n");
         for(i=0; i<=0xfaaa1eff; i++) {
         }
 }
